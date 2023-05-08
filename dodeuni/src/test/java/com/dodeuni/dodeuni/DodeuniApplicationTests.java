204f29ecd1fcfb105b2656e31ead893a279e4591
GaGa-Kim <wn8925@sookmyung.ac.kr>
2023-05-08 21:46:23 +0900

    init

---
+++ dodeuni/src/test/java/com/dodeuni/dodeuni/DodeuniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dodeuni.dodeuni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DodeuniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

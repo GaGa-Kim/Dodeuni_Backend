14473a645d4d79fb18f88c7023e328697366ba5f
GaGa-Kim <wn8925@sookmyung.ac.kr>
2023-05-11 16:00:05 +0900

    feat: Add Alarm Push API

--- dodeuni/src/main/java/com/dodeuni/dodeuni/web/AlarmController.java
+++ dodeuni/src/main/java/com/dodeuni/dodeuni/web/AlarmController.java
@@ -3,8 +3,10 @@ package com.dodeuni.dodeuni.web;
 import com.dodeuni.dodeuni.service.AlarmService;
 import com.dodeuni.dodeuni.web.dto.alarm.AlarmResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +21,11 @@ public class AlarmController {
     @GetMapping("/api/alarm")
     public ResponseEntity<List<AlarmResponseDto>> findCommentByCommunity(@RequestParam Long userId) {
         return ResponseEntity.ok().body(alarmService.findCommentByCommunity(userId));
+    }
+
+    @PostMapping("/api/alarm")
+    public ResponseEntity<Object> getFcmToken(@RequestParam Long userId, String fcmToken) {
+        alarmService.getFcmToken(userId, fcmToken);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
     }
 }

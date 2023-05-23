520b00430f9613f67268277e8f49136dcfd4be44
Choi Ga Hui <cherrycoke@sookmyung.ac.kr>
2023-05-23 09:39:29 +0900

    fix: Place Recommendation list query syntax error

--- dodeuni/src/main/java/com/dodeuni/dodeuni/domain/place/PlaceRepository.java
+++ dodeuni/src/main/java/com/dodeuni/dodeuni/domain/place/PlaceRepository.java
@@ -11,14 +11,14 @@ public interface PlaceRepository extends JpaRepository<Place, Long> {
     @Query("select new com.dodeuni.dodeuni.web.dto.place.PlaceListResponseDto(p.id, p.name, p.category, p.address, p.contact," +
             "p.x, p.y, p.createdDateTime, p.modifiedDateTime, u.id, u.nickname," +
             "(6371 * acos(cos(radians(:y)) * cos(radians(p.y)) * cos(radians(p.x) - radians(:x))" +
-            "+ sin(radians(:y)) * sin(radians(p.y)))) as distance, count(p.id) as reviewsCount) " +
+            "+ sin(radians(:y)) * sin(radians(p.y)))) as distance, count(r.place) as reviewsCount) " +
             "from Place p " +
             "left join p.user u " +
             "left join p.placeReviewList r " +
             "where p.address like %:keyword% " +
             "and (6371 * acos(cos(radians(:y)) * cos(radians(p.y)) * cos(radians(p.x) - radians(:x)) " +
             "+ sin(radians(:y)) * sin(radians(p.y)))) <= 1.5 " +
-            "group by p.id having r.place is not null order by distance desc")
+            "group by r.place having r.place is not null order by distance desc")
     List<PlaceListResponseDto> getPlaceByLocationAndKeyword(@Param("x") double x, @Param("y") double y, @Param("keyword") String keyword);
     Place findByNameAndAddressAndXAndY(String name, String address, double x, double y);
 }
